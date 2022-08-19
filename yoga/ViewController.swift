e6e5a8c91a0dcd40bf8249b8bf5ff7babc9990e6
melek <melekturudi3@gmail.com>
2022-08-19 20:40:50 +0300

    Initial Commit

---
+++ yoga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yoga
+//
+//  Created by melek türüdi on 19.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
